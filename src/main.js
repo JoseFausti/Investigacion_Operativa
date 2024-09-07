716df7fb624bd2da03149703ea74b92c047da1c0
JoseFausti <joseemilianofausti@gmail.com>
2024-09-07 14:25:23 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+function sum(a,b){
+    return a + b;
+}
+
+// Ejercicio 1
+function pin(pin) {
+    if (pin ==! null && Number.parseInt(pin) && (String.length(pin) >= 4 && String.length(pin) <= 6)) {
+
+    }
+}
+
+
+module.exports = {sum};
