e210dde77147bd4cf62dd24a38da9aaffbb74a27
JoseFausti <joseemilianofausti@gmail.com>
2024-09-13 19:07:37 -0300

    Actualizado

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ function sum(a,b){
     return a + b;
 }
 
+/** PARTE I **/
+
 // Ejercicio 1
 function pin(pin) {
     // Verificar que pin no sea nulo, sea una cadena numérica y tenga exactamente 4 o 6 caracteres
@@ -51,4 +53,62 @@ function calcularSiglo(anio) {
     return -1;
 }
 
-module.exports = {sum, pin, calcularVolumen, calcularSiglo};
+/** PARTE II **/
+
+// Ejercicio 3
+function paseoBicicleta(h){
+    if (!isNaN(h) && h !== null && h > 0) {
+        let l_agua = Math.floor(h * 0.5);
+        return l_agua;
+    }
+    return -1;
+}
+
+// Ejercicio 5
+function piedraPapelTijera(p1, p2) {
+    const reglas = {
+        piedra: "tijera",
+        papel: "piedra",
+        tijera: "papel"
+    }
+    if(typeof p1 === 'string' && typeof p2 === 'string'){
+        if ((p1 === 'piedra' || p1 === 'papel' || p1 === 'tijera') && (p2 === 'piedra' || p2 === 'papel' || p2 === 'tijera')) {
+            if(p1 === p2){
+                return "empate";
+            }
+            return reglas[p1] === p2 ? "jugador1 gana" : "jugador2 gana";
+        }
+    }
+    return -1;
+}
+
+// Ejercicio 6
+function validadorDirecciones(direccion) {
+    const requiredFields = {
+        street: 'string',
+        number: 'number',
+        zip_code: 'number',
+        city: 'string',
+        province: 'string'
+    };
+
+    // Parametros obligatorios
+    for (let field in requiredFields) {
+        if (!(field in direccion) || typeof direccion[field] !== requiredFields[field]) {
+            return -1;
+        }
+    }
+
+    // Parametros opcionales:
+    if (direccion.floor_apartment && typeof direccion.floor_apartment !== 'string') {
+        return -1; 
+    }
+    if (direccion.town && typeof direccion.town !== 'string') {
+        return -1; 
+    }
+
+    return direccion;
+}
+
+
+module.exports = {sum, pin, calcularVolumen, calcularSiglo, paseoBicicleta, piedraPapelTijera, validadorDirecciones};
--- src/sum.spec.js
+++ src/sum.spec.js
@@ -1,10 +1,13 @@
-// toBeCloseTo(3141.59, 2) -> (nº esperado, decimales a redondear)
+// toBeCloseTo(3141.59, 2) -> (nº esperado, decimales a redondear).
+// toEqual(): Compara objetos de manera profunda. En este caso, estamos comprobando que la función devuelva el mismo objeto de entrada.
 
-const { sum, pin, calcularVolumen, calcularSiglo} = require('./main');
+const { sum, pin, calcularVolumen, calcularSiglo, paseoBicicleta, piedraPapelTijera, validadorDirecciones} = require('./main');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+/** PARTE I **/
 
 // Ejercicio 1
 test('validate PIN code', ()=>{
@@ -41,4 +44,59 @@ test('calculate century from year', () => {
     expect(calcularSiglo(2000)).toBe(20);
     expect(calcularSiglo(1)).toBe(1);
     expect(calcularSiglo(101)).toBe(2);
+});
+
+
+/** PARTE II **/
+
+// Ejercicio 3
+test('water consume per hour', ()=>{
+    expect(paseoBicicleta(3)).toBe(1);
+    expect(paseoBicicleta(6.7)).toBe(3);
+    expect(paseoBicicleta(11.8)).toBe(5);
+    expect(paseoBicicleta(-4)).toBe(-1);
+    expect(paseoBicicleta('a')).toBe(-1);
+});
+
+// Ejercicio 5
+test('rock-paper-scissors game',()=>{
+    expect(piedraPapelTijera('tijera','papel')).toBe('jugador1 gana');
+    expect(piedraPapelTijera('papel','tijera')).toBe('jugador2 gana');
+    expect(piedraPapelTijera(3,'tijera')).toBe(-1);
+    expect(piedraPapelTijera('piedra',2)).toBe(-1);
+    expect(piedraPapelTijera('pie','tijera')).toBe(-1);
+});
+
+// Ejercicio 6
+test('validate directions', ()=>{
+    const direccionEjemplo = {
+        "street": "Salta",
+        "number": 359,
+        "floor_apartment": "D3",
+        "zip_code": 5500,
+        "town": "",
+        "city": "Mendoza",
+        "province": "Mendoza"
+    };
+    const direccionEjemplo2 = {
+        "street": "Salta",
+        "number": 359,
+        "floor_apartment": "",
+        "zip_code": 5500,
+        "town": "",
+        "city": "Mendoza",
+        "province": "Mendoza"
+    };
+    const direccionEjemplo3 = {
+        "street": "Salta",
+        "number": 359,
+        "floor_apartment": "D3",
+        "zip_code": "5500",
+        "town": "",
+        "city": "Mendoza",
+        "province": "Mendoza"
+    };
+    expect(validadorDirecciones(direccionEjemplo)).toEqual(direccionEjemplo); 
+    expect(validadorDirecciones(direccionEjemplo2)).toEqual(direccionEjemplo2); 
+    expect(validadorDirecciones(direccionEjemplo3)).toBe(-1);
 });
